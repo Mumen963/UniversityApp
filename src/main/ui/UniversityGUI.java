3a6e6b53f8ab35ae5fde3cc9e3bf47311c9daa4d
Mumen963 <134957743+Mumen963@users.noreply.github.com>
2023-11-23 01:21:53 -0800

    Add/Remove faculties and students in GUI

--- src/main/ui/UniversityGUI.java
+++ src/main/ui/UniversityGUI.java
@@ -1,26 +1,48 @@
 package ui;
 
 import model.Faculty;
+import model.Student;
 import model.University;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 public class UniversityGUI extends JFrame {
 
     private DefaultListModel<String> facultyListModel;
     private JList<String> facultyList;
+    private DefaultListModel<String> studentListModel;
+    private JList<String> studentList;
     private University university;
+    private Map<String, List<Student>> facultyStudentsMap;
+
 
     public UniversityGUI() {
 
         super("University GUI");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         university = new University("My University");
+
+        facultyStudentsMap = new HashMap<>();
+
         facultyListModel = new DefaultListModel<>();
         facultyList = new JList<>(facultyListModel);
+        facultyList.addListSelectionListener(e -> updateStudentList());
+
         JScrollPane facultyScrollPane = new JScrollPane(facultyList);
+
+        studentListModel = new DefaultListModel<>();
+        studentList = new JList<>(studentListModel);
+
+
+        JScrollPane studentScrollPane = new JScrollPane(studentList);
+
         JButton addFacultyButton = new JButton("Add Faculty");
         addFacultyButton.addActionListener(e -> {
             String facultyName = JOptionPane.showInputDialog("Enter faculty name:");
@@ -28,6 +50,7 @@ public class UniversityGUI extends JFrame {
                 Faculty faculty = new Faculty(facultyName);
                 if (university.addFaculty(faculty)) {
                     facultyListModel.addElement(facultyName);
+                    facultyStudentsMap.put(facultyName, new ArrayList<>());
                 } else {
                     JOptionPane.showMessageDialog(this, "Faculty already exists!");
                 }
@@ -39,29 +62,85 @@ public class UniversityGUI extends JFrame {
             int selectedIndex = facultyList.getSelectedIndex();
             if (selectedIndex != -1) {
                 String removedFacultyName = facultyListModel.remove(selectedIndex);
-                List<Faculty> allFaculties = university.getAllFaculties();
-                for (Faculty faculty : allFaculties) {
-                    if (faculty.getName().equals(removedFacultyName)) {
-                        allFaculties.remove(faculty);
-                        break;
+                facultyStudentsMap.remove(removedFacultyName);
+            }
+        });
+
+        JButton addStudentButton = new JButton("Add Student");
+        addStudentButton.addActionListener(e -> {
+            Faculty selectedFaculty = getSelectedFaculty();
+            if (selectedFaculty != null) {
+                String studentName = JOptionPane.showInputDialog("Enter student name:");
+                if (studentName != null && !studentName.isEmpty()) {
+                    double studentGpa = Double.parseDouble(JOptionPane.showInputDialog("Enter student GPA:"));
+                    Student student = new Student(studentName, studentGpa);
+                    List<Student> students = facultyStudentsMap.get(selectedFaculty.getName());
+                    if (students.add(student)) {
+                        studentListModel.addElement(studentName);
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Student already exists in the faculty!");
                     }
                 }
             }
         });
 
-        setLayout(new BorderLayout());
-        add(facultyScrollPane, BorderLayout.CENTER);
+        JButton removeStudentButton = new JButton("Remove Student");
+        removeStudentButton.addActionListener(e -> {
+            int selectedIndex = studentList.getSelectedIndex();
+            if (selectedIndex != -1) {
+                String removedStudentName = studentListModel.remove(selectedIndex);
+                Faculty selectedFaculty = getSelectedFaculty();
+                if (selectedFaculty != null) {
+                    List<Student> students = facultyStudentsMap.get(selectedFaculty.getName());
+                    students.removeIf(student -> student.getName().equals(removedStudentName));
+                }
+            }
+        });
 
+        setLayout(new BorderLayout());
+
+        JPanel facultyPanel = new JPanel(new BorderLayout());
+        facultyPanel.add(facultyScrollPane, BorderLayout.CENTER);
         JPanel facultyButtonPanel = new JPanel();
         facultyButtonPanel.add(addFacultyButton);
         facultyButtonPanel.add(removeFacultyButton);
-        add(facultyButtonPanel, BorderLayout.SOUTH);
+        facultyPanel.add(facultyButtonPanel, BorderLayout.SOUTH);
+        add(facultyPanel, BorderLayout.WEST);
 
-        setSize(400, 300);
+        JPanel studentPanel = new JPanel(new BorderLayout());
+        studentPanel.add(studentScrollPane, BorderLayout.CENTER);
+        JPanel studentButtonPanel = new JPanel();
+        studentButtonPanel.add(addStudentButton);
+        studentButtonPanel.add(removeStudentButton);
+        studentPanel.add(studentButtonPanel, BorderLayout.SOUTH);
+        add(studentPanel, BorderLayout.EAST);
+
+        setSize(800, 300);
         setVisible(true);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new UniversityGUI());
+    }
+
+
+    private void updateStudentList() {
+        studentListModel.clear();
+        Faculty selectedFaculty = getSelectedFaculty();
+        if (selectedFaculty != null) {
+            List<Student> students = facultyStudentsMap.get(selectedFaculty.getName());
+            for (Student student : students) {
+                studentListModel.addElement(student.getName());
+            }
+        }
+    }
+
+    private Faculty getSelectedFaculty() {
+        int selectedIndex = facultyList.getSelectedIndex();
+        if (selectedIndex != -1) {
+            String selectedFacultyName = facultyListModel.get(selectedIndex);
+            return new Faculty(selectedFacultyName);
+        }
+        return null;
     }
 }
